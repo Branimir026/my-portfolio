a9d2145e4b3dbfa9fb08833781dba0cd50dab423
Branimir026 <br.belic@gmail.com>
2021-06-10 14:27:16 +0200

    Changed welcome image to picture of myself

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import dino from "./dinoIdle1.png";
+import myImg from "./myself.png";
 
 const Welcome = () => {
   return (
     <Row id="welcome">
       <Col className="profilePicture" xs={12} md={6}>
-        <img src={dino} alt="Branimir Belić" />
+        <img src={myImg} alt="Branimir Belić" />
       </Col>
       <Col className="profile" xs={12} md={6}>
         <h2>
