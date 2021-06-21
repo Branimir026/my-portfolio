41a3fdc6786cfc0099b607171a2b6e27acbe87d8
Branimir026 <br.belic@gmail.com>
2021-06-21 15:29:52 +0200

    Added animate.css and made simple animations

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -7,10 +7,14 @@ const Welcome = () => {
   return (
     <Row id="welcome">
       <Col className="profilePicture" xs={12} md={6}>
-        <img src={myImg} alt="Branimir Belić" />
+        <img
+          className="animate__animated animate__fadeInLeft"
+          src={myImg}
+          alt="Branimir Belić"
+        />
       </Col>
       <Col className="profile" xs={12} md={6}>
-        <h2>
+        <h2 className="animate__animated animate__fadeInRight">
           I'm Branimir Belić <br /> web developer
         </h2>
       </Col>
