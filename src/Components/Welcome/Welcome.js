b25eb426647a472cf2acf3d7fb1b574105d78750
Branimir026 <br.belic@gmail.com>
2021-09-05 21:00:53 +0200

    Changed picture on home page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import myImg from "./myself.png";
+import myImg from "./branimir.png";
 
 const Welcome = () => {
   return (
