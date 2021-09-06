367f6dac9a23a27fa00129cf9949410257356dea
Branimir026 <br.belic@gmail.com>
2021-09-06 12:53:03 +0200

    Added particle component to welcome component, added methods for changing color on hover

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -2,10 +2,20 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import myImg from "./branimir.png";
+import ParticleBackground from "./ParticleBackground";
 
 const Welcome = () => {
+  const changeColorOnHover = (e) => {
+    e.target.style.color = "#ffd700";
+  };
+
+  const changeColorOnExit = (e) => {
+    e.target.style.color = "#d9cab3";
+  };
+
   return (
     <Row id="HOME">
+      <ParticleBackground />
       <Col className="profilePicture" xs={12} md={6}>
         <img
           className="animate__animated animate__fadeInLeft"
@@ -14,7 +24,11 @@ const Welcome = () => {
         />
       </Col>
       <Col className="profile" xs={12} md={6}>
-        <h2 className="animate__animated animate__fadeInRight">
+        <h2
+          className="animate__animated animate__fadeInRight"
+          onMouseOver={changeColorOnHover}
+          onMouseLeave={changeColorOnExit}
+        >
           I'm Branimir Belić <br /> web developer
         </h2>
       </Col>
