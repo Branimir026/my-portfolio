89af19f20aafa4d34ac946036639e0f683109495
Branimir026 <br.belic@gmail.com>
2021-09-07 11:48:21 +0200

    Added bootstrap container

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const Navigation = () => {
   const goTo = (e) => {
@@ -9,17 +10,19 @@ const Navigation = () => {
   };
 
   return (
-    <Row className="navigation justify-content-around">
-      <Col xs={4} md={4}>
-        <span onClick={goTo}>HOME</span>
-      </Col>
-      <Col xs={4} md={4}>
-        <span onClick={goTo}>ABOUT</span>
-      </Col>
-      <Col xs={4} md={4}>
-        <span onClick={goTo}>PROJECTS</span>
-      </Col>
-    </Row>
+    <Container>
+      <Row className="navigation justify-content-around">
+        <Col xs={4} md={4}>
+          <span onClick={goTo}>HOME</span>
+        </Col>
+        <Col xs={4} md={4}>
+          <span onClick={goTo}>ABOUT</span>
+        </Col>
+        <Col xs={4} md={4}>
+          <span onClick={goTo}>PROJECTS</span>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
