f9ad3f0782eec2aadc485c582b67ccb41c6f6faa
Branimir026 <br.belic@gmail.com>
2022-07-11 21:06:46 +0200

    Changed profile picture, and some minor design changes

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import myImg from "./branimir.png";
+import myImg from "./Branimir_Belic.png";
 import ParticleBackground from "./ParticleBackground";
 
 const Welcome = () => {
